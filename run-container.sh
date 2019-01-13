663a146131e098529d67ac076e53d6777d98d4f4
karlyan <karlyan.kamerer@gmail.com>
2019-01-13 16:14:47 +0100

    removed administration packages

--- run-container.sh
+++ run-container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo docker run -itp 80:80 \
     -v /net/nextcloud/webapp:/usr/share/webapps/nextcloud/ \
-    --entrypoint bash \
+    --entrypoint /entrypoint.sh \
     archlinux/nextcloud
